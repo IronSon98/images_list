e69a3b2df54906ecf6de323cf978819082d1a85f
IronSon98 <ironcsantanafilho@gmail.com>
2022-05-01 17:29:36 -0300

    implementação do switch para alterar tema para dark

--- lib/home/widgets/drawer_widget.dart
+++ lib/home/widgets/drawer_widget.dart
@@ -1,7 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:images_list/core/theme/theme_model.dart';
 import 'package:images_list/core/utils/colors.dart';
 import 'package:images_list/home/home_controller.dart';
 import 'package:images_list/home/widgets/drawer_item_widget.dart';
+import 'package:provider/provider.dart';
 
 class DrawerWidget extends StatelessWidget {
   const DrawerWidget({Key? key, required this.controller}) : super(key: key);
@@ -10,95 +12,91 @@ class DrawerWidget extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    final brightness = MediaQuery.of(context).platformBrightness;
-    return Container(
-      color: brightness == Brightness.light ? secondaryColor : darkPrimaryColor,
-      child: Column(
-        children: <Widget>[
-          Container(
-            color: brightness == Brightness.light
-                ? primaryColor
-                : darkPrimaryColor,
-            height: 28.0,
-          ),
-          Container(
-            height: 65.0,
-            width: double.maxFinite,
-            color: brightness == Brightness.light
-                ? primaryColor
-                : darkPrimaryColor,
-            child: Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 13.33),
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                children: <Widget>[
-                  Text(
-                    'Configurações',
-                    style: TextStyle(
-                      fontSize: 16.0,
-                      fontWeight: FontWeight.bold,
-                      color: brightness == Brightness.light
-                          ? darkPrimaryColor
-                          : primaryColor,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(left: 10.0),
-                    child: Container(
-                      decoration: BoxDecoration(
-                        shape: BoxShape.circle,
-                        boxShadow: [
-                          BoxShadow(
-                            color: brightness == Brightness.light
-                                ? primaryColor
-                                : darkGrey,
-                          ),
-                        ],
+    return Consumer(builder: (context, ThemeModel themeNotifier, child) {
+      return Container(
+        color: themeNotifier.isDark ? darkPrimaryColor : secondaryColor,
+        child: Column(
+          children: <Widget>[
+            Container(
+              color: themeNotifier.isDark ? darkPrimaryColor : primaryColor,
+              height: 28.0,
+            ),
+            Container(
+              height: 65.0,
+              width: double.maxFinite,
+              color: themeNotifier.isDark ? darkPrimaryColor : primaryColor,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(horizontal: 13.33),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    Text(
+                      'Configurações',
+                      style: TextStyle(
+                        fontSize: 16.0,
+                        fontWeight: FontWeight.bold,
+                        color: themeNotifier.isDark
+                            ? primaryColor
+                            : darkPrimaryColor,
                       ),
-                      child: IconButton(
-                        icon: Icon(
-                          Icons.close,
-                          color: brightness == Brightness.light
-                              ? darkGrey
-                              : primaryColor,
-                          size: 30.0,
+                    ),
+                    Padding(
+                      padding: const EdgeInsets.only(left: 10.0),
+                      child: Container(
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          boxShadow: [
+                            BoxShadow(
+                              color: themeNotifier.isDark
+                                  ? darkGrey
+                                  : primaryColor,
+                            ),
+                          ],
                         ),
-                        onPressed: () {
-                          Navigator.pop(context);
-                        },
+                        child: IconButton(
+                          icon: Icon(
+                            Icons.close,
+                            color:
+                                themeNotifier.isDark ? primaryColor : darkGrey,
+                            size: 30.0,
+                          ),
+                          onPressed: () {
+                            Navigator.pop(context);
+                          },
+                        ),
                       ),
                     ),
-                  ),
-                ],
+                  ],
+                ),
               ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.all(10.0),
-            child: Card(
-              child: ListView.builder(
-                shrinkWrap: true,
-                itemCount: controller.lengthSettings,
-                itemBuilder: (_, index) {
-                  return Column(
-                    children: <Widget>[
-                      DrawerItemWidget(
-                        item: controller.listSettings[index],
-                      ),
-                      Visibility(
-                        visible: (index + 1) == controller.lengthSettings
-                            ? false
-                            : true,
-                        child: const Divider(),
-                      ),
-                    ],
-                  );
-                },
+            Padding(
+              padding: const EdgeInsets.all(10.0),
+              child: Card(
+                child: ListView.builder(
+                  shrinkWrap: true,
+                  itemCount: controller.lengthSettings,
+                  itemBuilder: (_, index) {
+                    return Column(
+                      children: <Widget>[
+                        DrawerItemWidget(
+                          item: controller.listSettings[index],
+                        ),
+                        Visibility(
+                          visible: (index + 1) == controller.lengthSettings
+                              ? false
+                              : true,
+                          child: const Divider(),
+                        ),
+                      ],
+                    );
+                  },
+                ),
               ),
             ),
-          ),
-        ],
-      ),
-    );
+          ],
+        ),
+      );
+    });
   }
 }
